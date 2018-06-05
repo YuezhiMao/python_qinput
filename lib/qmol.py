922292bd7a3e531a56a426c94b7cc85ad7186cd3
Yuezhi Mao <YMao@bowie.cchem.berkeley.edu>
2018-06-04 18:55:43 -0700

    new script and lib

---
+++ lib/qmol.py
@@ -0,0 +1,104 @@
+import os, glob, re, sys
+import subprocess as sp
+import numpy as np
+
+class XYZ:  #class for XYZ coordinates
+   def __init__(self, xyz_file):
+      #print xyz_file
+      self.Name = re.search("([^/]+).xyz$", xyz_file).group(1)
+      f = open(xyz_file, 'r')
+      self.AtomList = []
+      self.CoordList = []
+      for line in f.readlines():
+         l = re.search('^\s*(\d+)\s*$', line)
+         if l!=None:
+            self.NAtom = int(l.group(1))
+         l = re.search('(\S+)\s+(\S+)\s+(\S+)\s+(\S+)', line)
+         if l!=None:
+            self.AtomList.append(l.group(1))
+            coord = float(l.group(2)), float(l.group(3)), float(l.group(4))
+            self.CoordList.append(coord)
+
+      if(len(self.AtomList)!=self.NAtom):
+         print "Error in number of atoms"
+         sys.exit(1)
+
+class FRGM: #class for fragment partition information
+   def __init__(self, frgm_file):
+      fr = open(frgm_file, 'r')
+      line = f.readline()
+      counter = 0
+      while line != '':
+         counter = counter + 1
+         ls = line.split()
+         if counter == 1:
+            if len(ls) != 1:
+               print "Error in line %d of %s" %(counter, frgm_file)
+               sys.exit(0)
+            else:
+               self.total_charge = int(ls[0])
+         elif counter == 2:
+            if len(ls) != 1:
+               print "Error in line %d of %s" %(counter, frgm_file)
+               sys.exit(0)
+            else:
+               self.total_mult = int(ls[0])
+         elif counter == 3:
+            self.n_frgm = len(ls)
+            if self.n_frgm < 2:
+               print "Error in line %d of %s: At least two fragments required" %(counter, frgm_file)
+               sys.exit(0)
+            self.charge_frgm = [int(i) for i in ls]
+         elif counter == 4:
+            if len(ls) != self.n_frgm:
+               print "Error in line %d of %s" %(counter, frgm_file)
+               sys.exit(0)
+            self.mult_frgm = [int(i) for i in ls]
+         elif counter == 5:
+            if len(ls) != self.n_frgm:
+               print "Error in line %d of %s" %(counter, frgm_file)
+               sys.exit(0)
+            self.natoms_frgm = [int(i) for i in ls] 
+            
+         else:
+            print "skip the unexpected lines in " + frgm_file 
+         line = f.readline()
+
+      self.atoms_offset = np.zeros(self.n_frgm, dtype=int)
+      offset = 0
+      for index in range(0, self.n_frgm):
+         self.atoms_offset[index] = offset
+         offset += self.natoms_frgm[index] 
+
+def WriteMolecule(fw, XYZ, charge, mult):
+   fw.write('$molecule\n')
+   #total charge and mult
+   fw.write("%d %d\n" %(charge, mult))
+   for iatom in range(0, XYZ.NAtom):
+      atomic_symbol = XYZ.AtomList[iatom]
+      x, y, z = XYZ.CoordList[iatom]
+      fw.write("%-4s %-10.5f %-10.5f %-10.5f\n" %(atomic_symbol, x, y, z))
+   fw.write('$end\n')
+   fw.write('\n')
+
+def WriteMolecule_Frgm(fw, XYZ, FRGM):
+   fw.write('$molecule\n')
+   #total charge and mult
+   fw.write("%d %d\n" %(FRGM.total_charge, FRGM.total_mult))
+   #loop over fragments
+   for ifrgm in range(0, FRGM.n_frgm):
+      fw.write('--\n')
+      fw.write("%d %d\n" %(FRGM.charge_frgm[ifrgm], FRGM.mult_frgm[ifrgm]))
+      natom_frgm = FRGM.natoms_frgm[ifrgm]
+      index_start = FRGM.atoms_offset[ifrgm]
+      for iatom in range(index_start, index_start+natom_frgm):
+         atomic_symbol = XYZ.AtomList[iatom]
+         x, y, z = XYZ.CoordList[iatom]
+         fw.write("%-4s %-10.5f %-10.5f %-10.5f\n" %(atomic_symbol, x, y, z))
+   fw.write('$end\n')
+   fw.write('\n')
+
+def WriteMolecule_Read(fw):
+   fw.write('$molecule\n')
+   fw.write('read\n')
+   fw.write('$end\n\n')
--- lib/qrems.py
+++ lib/qrems.py
@@ -52,3 +52,91 @@ def copy_section_over(fw, filename):
    for line in fr.readlines():
       fw.write(line)
    fr.close()
+
+def basis_abbr(BasName):    #generating abbreviated name for basis sets, using for input file names
+   fullname = BasName.lower()
+   if fullname == 'cc-pvdz':
+      return 'dz'
+   elif fullname == 'cc-pvtz':
+      return 'tz'
+   elif fullname == 'cc-pvqz':
+      return 'qz'
+   elif fullname == 'aug-cc-pvdz':
+      return 'adz'
+   elif fullname == 'aug-cc-pvtz':
+      return 'atz'
+   elif fullname == 'aug-cc-pvqz':
+      return 'aqz'
+   elif fullname == 'def2-svpd' or fullname == 'svpd':
+      return 'svpd'
+   elif fullname == 'def2-tzvpp' or fullname == 'tzvpp':
+      return 'tzvpp'
+   elif fullname == 'def2-tzvppd' or fullname == 'tzvppd':
+      return 'tzvppd'
+   elif fullname == 'def2-qzvpp' or fullname == 'qzvpp':
+      return 'qzvpp'
+   elif fullname == 'def2-qzvppd' or fullname == 'qzvppd':
+      return 'qzvppd'
+   elif fullname == 'pc-2':
+      return 'pc2'
+   elif fullname == 'pc-3':
+      return 'pc3'
+   elif fullname == 'aug-pc-2':
+      return 'apc2'
+   elif fullname == 'aug-pc-3':
+      return 'apc3'
+   elif fullname == 'dp':
+      return 'dp'
+   elif fullname == 'tp':
+      return 'tp' 
+   elif fullname == 'lp':
+      return 'lp'
+   else:
+      print "unrecognized basis choice"
+      sys.exit(1)
+
+def set_rems_common(curREM, method, basis):
+   if basis.upper() == 'SVPD':
+      ModRem('BASIS','DEF2-SVPD',curREM)
+   elif basis.upper() == 'TZVPP':
+      ModRem('BASIS','DEF2-TZVPP',curREM)
+   elif basis.upper() == 'TZVPPD':
+      ModRem('BASIS','DEF2-TZVPPD',curREM)
+   elif basis.upper() == 'QZVPP':
+      ModRem('BASIS','DEF2-QZVPP',curREM)
+   elif basis.upper() == 'QZVPPD':
+      ModRem('BASIS','DEF2-QZVPPD',curREM)
+   elif basis.upper() == 'DP': #double-zeta Pople: 6-31+G(d)
+      ModRem('BASIS','6-31+G(d)', curREM)
+   elif basis.upper() == 'TP': #triple-zeta Pople: 6-311++G(2df,2pd)
+      ModRem('BASIS','6-311++G(2df,2pd)',curREM)
+   elif basis.upper() == 'LP':
+      ModRem('BASIS','6-311++G(3df,3pd)', curREM)
+   else: #copy the name of standard basis
+      ModRem('BASIS', basis, curREM)
+   ModRem('MEM_TOTAL', '8000', curREM)
+   ModRem('MEM_STATIC','2000', curREM)
+   ModRem('SCF_GUESS', 'SAD', curREM)
+   ModRem('METHOD', method, curREM)	
+   if (method != 'HF'):
+      ModRem ('XC_GRID', '000099000590', curREM)		
+      ModRem ('NL_GRID', '1', curREM)
+   #deal with -D3 in the method
+   if "-D3" in method.upper():
+      add_d3_tail(method, curREM)
+
+def add_d3_tail(method, curREM):
+   if method.upper() == 'WB97X-D3' or method.upper() == 'B97-D3':
+      return
+   l = re.search('(\S+)-(D3\S*)', method)
+   parent_func = l.group(1)
+   tail = l.group(2)
+   ModRem('METHOD', parent_func, curREM)
+   if 'BJ' in tail.upper():
+      ModRem('DFT_D', 'D3_BJ', curREM)
+   elif 'OP' in tail.upper():
+      ModRem('DFT_D', 'D3_OP', curREM)
+   elif 'CSO' in tail.upper():
+      ModRem('DFT_D', 'D3_CSO', curREM)
+   else:
+      ModRem('DFT_D', 'EMPIRICAL_GRIMME3', curREM)
