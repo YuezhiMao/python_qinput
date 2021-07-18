acdb8958f8de78b97f28127b81e780cff778f5ff
Yuezhi Mao <myzpku@gmail.com>
2021-07-17 22:56:44 -0700

    2to3 conversion for all scripts

--- checkScratch.py
+++ checkScratch.py
@@ -3,7 +3,7 @@ import os
 max_host_number = 102
 host_list = ["n0{:03d}.mhg".format(x) for x in range(30, max_host_number)]
 
-print "there are {} MHG nodes".format(max_host_number)
+print("there are {} MHG nodes".format(max_host_number))
 for host in host_list:
    os.system("echo {}".format(host))
    #os.system("ssh {} \"df -h \" ".format(host))
